9f1df0b8cb3a428797aa9c66e06e755d3f811fc8
BIJIN03 <bijineliasmathew@gmail.com>
2024-12-25 20:48:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import speech_recognition as sr
+import webbrowser
+import requests
+import pygame
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+from gtts import gTTS
+from music import musics
+
+# import pyttsx3
+
+load_dotenv()
+
+# engine = pyttsx3.init()
+
+newsapi=os.getenv("NEWS_API")
+geminiapi=os.getenv("GEMINI_API")
+
+def speak(mytext):
+    
+    # engine.say(text)
+    # engine.runAndWait()
+
+    speech = gTTS(text=mytext,slow=False)
+    speech.save("text.mp3")
+    pygame.mixer.init()
+    pygame.mixer.music.load("text.mp3")
+    pygame.mixer.music.play()
+    # while pygame.mixer.music.get_busy():  
+    #     pygame.time.Clock().tick(10)
+
+def aiprocess(command):
+
+    genai.configure(api_key=geminiapi)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(f"Give a brief description on {command}")
+    return response.text
+
+def processCommand(c):
+
+    if "open" in c.lower():
+        site=c.lower().split(" ")[1]
+        speak(f"Opening {site}.")
+        webbrowser.open(f"https://www.{site}.com")
+    
+    elif c.lower().startswith("play"):
+        song=c.lower().strip("play ")
+        speak(f"Playing {song}")
+        webbrowser.open(musics[song])
+    elif "news" in c.lower():
+        res=requests.get(f"https://newsapi.org/v2/everything?q=tesla&from=2024-11-25&sortBy=publishedAt&apiKey={newsapi}")
+
+        if res.status_code==200:
+            data=res.json()
+            articles=data.get("articles",[])
+            for article in articles:
+                speak(article["title"])
+    else:
+        result=aiprocess(c)
+        speak(result)
+    
+
+if __name__=="__main__":
+    speak("Initialising Jarvis...")
+    r = sr.Recognizer()
+    while True:
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio=r.listen(source,timeout=2,phrase_time_limit=1)
+            word=r.recognize_google(audio)
+            print(word)
+            if "jarvis" in word.lower():
+                speak("Yes,How can I help you?")
+                print("Jarvis Active.")
+                try:
+                    with sr.Microphone() as source:
+                        audio=r.listen(source)
+                
+                    command=r.recognize_google(audio)
+                    print(command)
+                    if command.lower() == "exit" or command.lower()=="stop":
+                        speak("Jarvis shutting down")
+                        break
+                    processCommand(command)
+
+                except :
+                    print("No command recieved.")
+                    
+        except Exception as e:
+            print("Speak Something.")
---
+++ music.py
@@ -0,0 +1,5 @@
+musics={
+    "skyfall":"https://www.youtube.com/watch?v=sZrTJesvJeo",
+    "despacito":"https://www.youtube.com/watch?v=gm3-m2CFVWM",
+    "summertime sadness":"https://www.youtube.com/watch?v=TdrL3QxjyVw"
+}
